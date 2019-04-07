a535237e0b2fd6a2878339c5860d5c8bc0301568
hzh0512 <hzh0512@gmail.com>
2019-04-07 19:41:02 -0400

    attack on cifar10

---
+++ attack_cifar10/attack.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+import numpy as np
+import matplotlib.pyplot as plt
+from cifar import model as cifar_model
+import torchvision
+
+
+epsilons = [0,0.01,0.02,0.03,0.04,0.05]
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+     ])
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+test_loader = torch.utils.data.DataLoader(testset, batch_size=1,
+                                         shuffle=True, num_workers=1)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+# Define what device we are using
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Initialize the network
+model = cifar_model.cifar10(128, True).to(device)
+
+# Set the model in evaluation mode. In this case this is for the Dropout layers
+model.eval()
+
+
+# FGSM attack code
+def fgsm_attack(image, epsilon, data_grad):
+    # Collect the element-wise sign of the data gradient
+    sign_data_grad = data_grad.sign()
+    # Create the perturbed image by adjusting each pixel of the input image
+    perturbed_image = image + epsilon*sign_data_grad
+    # Adding clipping to maintain [0,1] range
+    perturbed_image = torch.clamp(perturbed_image, 0, 1)
+    # Return the perturbed image
+    return perturbed_image
+
+
+def test( model, device, test_loader, epsilon ):
+
+    # Accuracy counter
+    correct = 0
+    adv_examples = []
+
+    # Loop over all examples in test set
+    for data, target in test_loader:
+
+        # Send the data and label to the device
+        data, target = data.to(device), target.to(device)
+
+        # Set requires_grad attribute of tensor. Important for Attack
+        data.requires_grad = True
+
+        # Forward pass the data through the model
+        output = model(data)
+        init_pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+
+        # If the initial prediction is wrong, dont bother attacking, just move on
+        if init_pred.item() != target.item():
+            continue
+
+        # Calculate the loss
+        loss = F.nll_loss(output, target)
+
+        # Zero all existing gradients
+        model.zero_grad()
+
+        # Calculate gradients of model in backward pass
+        loss.backward()
+
+        # Collect datagrad
+        data_grad = data.grad.data
+
+        # Call FGSM Attack
+        perturbed_data = fgsm_attack(data, epsilon, data_grad)
+
+        # Re-classify the perturbed image
+        output = model(perturbed_data)
+
+        # Check for success
+        final_pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+        if final_pred.item() == target.item():
+            correct += 1
+            # Special case for saving 0 epsilon examples
+            if (epsilon == 0) and (len(adv_examples) < 5):
+                adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
+                adv_examples.append( (classes[init_pred.item()], classes[final_pred.item()], adv_ex) )
+        else:
+            # Save some adv examples for visualization later
+            if len(adv_examples) < 5:
+                adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
+                adv_examples.append( (classes[init_pred.item()], classes[final_pred.item()], adv_ex) )
+
+        if len(adv_examples) == 5:
+            break
+
+    # Calculate final accuracy for this epsilon
+    final_acc = correct/float(len(test_loader))
+    print("Epsilon: {}\tTest Accuracy = {} / {} = {}".format(epsilon, correct, len(test_loader), final_acc))
+
+    # Return the accuracy and an adversarial example
+    return final_acc, adv_examples
+
+
+if __name__ == '__main__':
+    accuracies = []
+    examples = []
+
+    # Run test for each epsilon
+    for eps in epsilons:
+        acc, ex = test(model, device, test_loader, eps)
+        accuracies.append(acc)
+        examples.append(ex)
+
+    # Plot several examples of adversarial samples at each epsilon
+    cnt = 0
+    plt.figure(figsize=(8,10))
+    for i in range(len(epsilons)):
+        for j in range(len(examples[i])):
+            cnt += 1
+            plt.subplot(len(epsilons),len(examples[0]),cnt)
+            plt.xticks([], [])
+            plt.yticks([], [])
+            if j == 0:
+                plt.ylabel("Eps: {}".format(epsilons[i]), fontsize=14)
+            orig,adv,ex = examples[i][j]
+            plt.title("{} -> {}".format(orig, adv))
+            ex = np.transpose(ex, [1,2,0])
+            ex = (ex - ex.min()) / (ex.max() - ex.min())
+            plt.imshow(ex)
+    plt.tight_layout()
+    plt.show()
+
