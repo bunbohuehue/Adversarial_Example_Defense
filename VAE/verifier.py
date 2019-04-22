00fd7eeb0cacc2e45e61b080cf5a161f53047218
Haomin Wang <haominw@andrew.cmu.edu>
2019-04-21 20:42:57 -0400

    Add files via upload

---
+++ VAE/verifier.py
@@ -0,0 +1,163 @@
+import torch
+import torchvision
+from torch import nn
+from torch.autograd import Variable
+from torchvision import datasets,transforms
+from torchvision.utils import save_image
+from torchvision.datasets import MNIST
+import torch.nn.functional as F
+import torch.utils.data.dataloader as dataloader
+from random import randint
+import os
+import time
+import attack
+from verify_dataset import verifyDataset, pretrained_model
+
+b_size = 64
+total_b = 60000/64
+cu = torch.cuda.is_available()
+device = torch.device("cuda:0" if cu else "cpu")
+# LeNet Model definition
+class Net(nn.Module):
+	def __init__(self):
+		super(Net, self).__init__()
+		self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+		self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+		self.conv2_drop = nn.Dropout2d()
+		self.fc1 = nn.Linear(320, 50)
+		self.fc2 = nn.Linear(50, 10)
+
+	def forward(self, x):
+		x = F.relu(F.max_pool2d(self.conv1(x), 2))
+		x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+		x = x.view(-1, 320)
+		x = F.relu(self.fc1(x))
+		x = F.dropout(x, training=self.training)
+		x = self.fc2(x)
+		return F.log_softmax(x, dim=1)
+
+class autoencoder(nn.Module):
+    def __init__(self):
+        super(autoencoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 16, 5, stride=2, padding=2),  # b, 16, 14, 14
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+            nn.Conv2d(16, 32, 5, stride=2, padding=2),  # b, 32, 7, 7
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+        )
+        self.mean = nn.Linear(7 * 7 * 32 + 10, 128, bias=True)
+        self.std = nn.Linear(7 * 7 * 32 + 10, 128, bias=True)
+
+        self.fc = nn.Sequential(nn.Linear(128 + 10, 7 * 7 * 32, bias=True), nn.ReLU(inplace=True))
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(32, 16, 5, stride=2, padding=2, output_padding=1),  # b, 16, 14, 14
+            nn.ReLU(True),
+            nn.ConvTranspose2d(16, 1, 5, stride=2, padding=2, output_padding=1),  # b, 1, 28, 28
+            nn.Tanh()
+        )
+
+    def reparametrize(self, mu, logvar):
+        std = logvar.mul(0.5).exp_()
+        if torch.cuda.is_available():
+            eps = torch.cuda.FloatTensor(std.size()).normal_()
+        else:
+            eps = torch.FloatTensor(std.size()).normal_()
+        eps = Variable(eps)
+        return eps.mul(std).add_(mu)
+
+    def forward(self, x, y):
+        x = self.encoder(x)  # b x (7*7*32)
+        x = x.view(x.size(0), -1)
+        x = torch.cat([x, y], 1)  # b x (7*7*32 + 10)
+        mu, logvar = self.mean(x), self.std(x)
+        z = self.reparametrize(mu, logvar)
+        z = torch.cat([z, y], 1)
+        z = self.fc(z)
+        x = self.decoder(z.view(z.size(0), 32, 7, 7)), mu, logvar
+        return x
+
+class Verifier(nn.Module):
+    """
+    Modified from LeNet5
+    Input - batch_size x 2 x 28 x 28
+    Output - batch_size x 2
+    """
+    def __init__(self):
+        super(Verifier, self).__init__()
+
+        self.convnet = nn.Sequential(
+            nn.Conv2d(2, 6, kernel_size=(5, 5)),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2),
+            nn.Conv2d(6, 16, kernel_size=(5, 5)),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2),
+            nn.Conv2d(16, 120, kernel_size=(4, 4)),
+            nn.ReLU())
+
+        self.fc = nn.Sequential(
+            nn.Linear(120, 84),
+            nn.ReLU(),
+            nn.Linear(84, 2))
+
+    def forward(self, img):
+        output = self.convnet(img)
+        output = output.view(img.size(0), -1)
+        output = self.fc(output)
+        return output
+
+def train_epoch(model, train_loader, criterion, optimizer):
+    model.train()
+    model.to(device)
+    start_time = time.time()
+    start_time1 = time.time()
+    total_predictions = 0.0
+    correct_predictions = 0.0
+    running_loss = 0.0
+    for batch_idx, (data, target) in enumerate(train_loader):
+        optimizer.zero_grad()
+        data = data.to(device)
+        target = target.long().to(device)
+
+        outputs = model(data)
+        _, predicted = torch.max(F.softmax(outputs, dim=1), 1)
+        #print ('target: ', target)
+        #print ('predicted: ', predicted)
+        correct_predictions += (predicted == target).sum().item()
+        total_predictions += target.size(0)
+        loss = criterion(outputs, target)
+        running_loss += loss.item()
+
+        loss.backward()
+        optimizer.step()
+
+        torch.cuda.empty_cache()
+        end_time1 = time.time()
+        if batch_idx > 0 and batch_idx % 100 == 0:
+            print ('{:.2f} %, about {:.2f} minutes left.'.format(batch_idx*100*b_size/60000, (end_time1 - start_time1)*(total_b-batch_idx)/6000))
+            start_time1 = time.time()
+
+    acc = (correct_predictions/total_predictions)*100.0
+    end_time = time.time()
+    running_loss /= len(train_loader)
+    print('Training Loss: ', running_loss, 'Accuracy: ', acc, 'Running one epoch took',end_time - start_time, 's')
+
+
+CVAE = autoencoder().to(device)
+CVAE.load_state_dict(torch.load('curr_model_99.pth', map_location='cpu'))
+classifier = Net().to(device)
+classifier.load_state_dict(torch.load(pretrained_model, map_location='cpu'))
+verifier = Verifier()
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(verifier.parameters())
+num_epochs = 10
+
+verify_dataset = verifyDataset(CVAE, classifier)
+dataloader_args1 = dict(shuffle=True, batch_size=b_size, num_workers = 8, pin_memory=True) if cu \
+                        else dict(shuffle=True, batch_size=b_size)
+verify_loader = torch.utils.data.DataLoader(verify_dataset,**dataloader_args1)
+
+for epoch in range(num_epochs):
+    train_epoch(verifier, verify_loader, criterion, optimizer)
+    tosave = str(epoch+1) + 'epoch.pt'
+    torch.save(verifier, tosave)
--- VAE/verify_dataset.py
+++ VAE/verify_dataset.py
@@ -4,67 +4,62 @@ import torch.nn.functional as F
 import torch.utils.data.dataloader as dataloader
 import conv_cVAE
 from random import randint
+from torchvision.datasets import MNIST
+from torchvision import transforms
 
 epsilons = [0, .05, .1, .15, .2, .25, .3]
 pretrained_model = "../data/lenet_mnist_model.pth"
-use_cuda=True
+use_cuda = True
 
-# usage:
-# from verify_dataset import verifyDataset
-# dataloader_args = dict(shuffle=True, batch_size=64, pin_memory=True
-# verify_Dataset = verifyDataset(model, train_loader)
-# verify_loader = dataloader.DataLoader(verify_Dataset, **dataloader_args)
+
+img_transform = transforms.ToTensor()
 
 class verifyDataset(torch.utils.data.Dataset):
-	def __init__(self, VAE_model, classifier, train_loader):
-		device = torch.device("cuda" if (use_cuda and torch.cuda.is_available()) else "cpu")
-		examples, labels = generate(VAE_model, classifier, device, train_loader)
-		self.data = examples
-		self.label = labels
+	def __init__(self, VAE_model, classifier):
+		self.dataset = MNIST('./', train=True, transform=img_transform, download=True)
+		self.VAE_model = VAE_model
+		self.classifier = classifier
 
 	def __len__(self):
-		return len(self.data)
+		return len(self.dataset)
 
 	def __getitem__(self, i):
-		return self.data[i], self.label[i]
+		return self.generate(*self.dataset[i])
 
-
-def generate(VAE_model, classifier, device, test_loader):
-	adv_examples = []
-	adv_labels = []
-
-	# Loop over all examples in test set
-	for data, target in test_loader:
-
+	def generate(self, data, target):
+		data = data.unsqueeze(0)
+		target = target.unsqueeze(0)
 		# Send the data and label to the device
-		data, target = data.to(device), target.to(device)
 		data.requires_grad = True
 
 		# Forward pass the data through the model
-		output = classifier(data)
+		output = self.classifier(data)
+		original_pred = output.max(1, keepdim=True)[1]
 
 		loss = F.nll_loss(output, target)
-		model.zero_grad()
+		self.classifier.zero_grad()
 		loss.backward()
 		data_grad = data.grad.data
 
-		# Call FGSM Attack
-		epsilon = epsilons[randint(1, len(epsilons) - 1)]
-		# generate adversarial example
-		adv_example = attack.fgsm_attack(data, epsilon, data_grad)
+		if (randint(0, 4) > 1):
+			# Call FGSM Attack
+			epsilon = epsilons[randint(3, len(epsilons) - 1)]
+			# generate adversarial example
+			adv_example = attack.fgsm_attack(data, epsilon, data_grad)
+			# classify adv example
+			output = self.classifier(adv_example)
 
-		# classify adv example
-		output = classifier(adv_example)
+			# Check for success
+			adv_pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
 
-		# Check for success
-		final_pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
-		# if predicted label is true
-		if final_pred.item() == target.item():
-			adv_labels.append(True)
+			label = (adv_pred == target)[0].item()
+			gen_adv_data = (conv_cVAE.gen_image(self.VAE_model, adv_pred))
+			example = torch.cat((gen_adv_data, adv_example),1)
 		else:
-			adv_labels.append(False)
+			# don't call FGSM
+			label = (original_pred == target)[0].item()
+			# cVAE generated image
+			gen_data = (conv_cVAE.gen_image(self.VAE_model, original_pred))
+			example = torch.cat((gen_data, data),1)
 
-		gen_data = (conv_cVAE.gen_image(VAE_model, final_pred, 1))
-		adv_examples.append((data, gen_data))
-
-	return adv_examples, adv_labels
+		return example.squeeze(0), label
